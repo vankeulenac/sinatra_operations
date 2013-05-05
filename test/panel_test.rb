e9798665aaa57343e7c5818b4caa9cb225b92294
Yeehaa <medialoperations@gmail.com>
2013-05-05 10:43:33 +0200

    refactor tests and code into separate files

--- test/panel_test.rb
+++ test/panel_test.rb
@@ -9,11 +9,21 @@ describe Panel do
     end
   end
 
+  context "#self.from_hash" do   
+    it "should add_panel to panels array" do
+			size = panel_list
+			Panel.from_hash(p1) 
+      Panel.all.size.must_equal size + 1
+    end
+  end
+
   context "#to_json" do
     it "must output the original hash as json" do
       panel.to_json.must_equal p1.to_json
     end	
+  end
 
+  context "#self.to_json" do
     it "must output all the class instances as json" do
       panel_list
       Panel.to_json.must_equal Panel.all.map(&:to_json)
